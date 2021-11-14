d77f4df00e8181d876349107d95083eccfd46c62
Christoph Mussenbrock <christoph@etherisc.com>
2021-11-14 17:55:43 +0100

    Fix deployment

--- migrations-available/113_deploy_CLFlightStatusesOracle.js
+++ migrations-available/113_deploy_CLFlightStatusesOracle.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const gif = require('@etherisc/gif-connect')
-const { utils: { formatBytes32String } } = require('ethers')
 const truffleConfig = require('../truffle-config')
 
 const FlightStatusesOracle = artifacts.require('oracles/CLFlightStatusesOracle.sol')
@@ -16,7 +15,7 @@ module.exports = async (deployer, network /* , accounts */) => {
 
   const chainLinkTokenAddress = '0xE2e73A1c69ecF83F464EFCE6A5be353a37cA09b2'
   const chainLinkPaymentAmount = 0
-  const chainLinkJobId = '9f87b3276cbd4651b8a9180b26171c7b'
+  const chainLinkJobId = '0x9f87b3276cbd4651b8a9180b26171c7b'
   const chainLinkOracleAddress = '0xa68bC2d344f69F34f5A7cbb5233f9bF1a270B2f6'
 
   // Deploy FlightRatingsOracle
@@ -26,12 +25,12 @@ module.exports = async (deployer, network /* , accounts */) => {
     chainLinkOracleAddress,
     oracleServiceAddress,
     oracleOwnerServiceAddress,
-    formatBytes32String(chainLinkJobId),
+    chainLinkJobId,
     chainLinkPaymentAmount,
     {
-      gas: 4000000,
+      gas: 6000000,
     },
   )
 
-  info(`Deployed FlightRatingsOracle at ${flightStatusesOracle.address}`)
+  info(`Deployed FlightStatusesOracle at ${flightStatusesOracle.address}`)
 }
